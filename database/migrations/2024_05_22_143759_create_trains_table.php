9e9e0cb0d61e3c9306b8179db970444a7d1429c5
gianlucafalaschi <gian100gian@outlook.it>
2024-05-22 17:28:59 +0200

    columns  created

---
+++ database/migrations/2024_05_22_143759_create_trains_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time', $precision = 0);
+            $table->time('arrival_time', $precision = 0);
+            $table->string('train_code', 100);
+            $table->tinyInteger('number_of_wagons')->unsigned();
+            $table->tinyInteger('in_time');
+            $table->tinyInteger('cancelled')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
+
+
+/* 
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato 
+*/
