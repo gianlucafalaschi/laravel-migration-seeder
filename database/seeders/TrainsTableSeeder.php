45d237b999f85e94c1279dea4e397a7103be4921
gianlucafalaschi <gian100gian@outlook.it>
2024-05-22 19:53:10 +0200

    fakers data (not completed)

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +14,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        // ciclo for per 10 volte crea  una nuova istanza di treno, la popola e salva
+        for($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->city();
+            $newTrain->arrival_time = $faker->city();
+            $newTrain->train_code = $faker->city();
+            $newTrain->number_of_wagons = $faker->city();
+            $newTrain->in_time = $faker->city();
+            $newTrain->cancelled = $faker->city();
+
+        }
     }
 }
