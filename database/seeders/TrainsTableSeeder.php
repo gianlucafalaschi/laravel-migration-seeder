817228bae53a9dd97e936583d1bf152b36a361ca
gianlucafalaschi <gian100gian@outlook.it>
2024-05-23 10:13:27 +0200

    randomElements instead of randomElement, fixed. - Db populated

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -19,15 +19,15 @@ class TrainsTableSeeder extends Seeder
         // ciclo for per 10 volte crea  una nuova istanza di treno, la popola e salva
         for($i = 0; $i < 10; $i++) {
             $newTrain = new Train();
-            $newTrain->company = $faker->randomElements(['FS', 'FCU']);
+            $newTrain->company = $faker->randomElement(['FS', 'FCU']);
             $newTrain->departure_station = $faker->city();
             $newTrain->arrival_station = $faker->city();
             $newTrain->departure_time = $faker->time();
             $newTrain->arrival_time = $faker->time();
             $newTrain->train_code = $faker->randomNumber(5, true);
             $newTrain->number_of_wagons = $faker->numberBetween(1, 50);
-            $newTrain->in_time = $faker->randomElements(['0', '1']);
-            $newTrain->cancelled = $faker->randomElements(['0', '1']);
+            $newTrain->in_time = $faker->randomElement(['0', '1']);
+            $newTrain->cancelled = $faker->randomElement(['0', '1']);
             $newTrain->save();
 
         }
